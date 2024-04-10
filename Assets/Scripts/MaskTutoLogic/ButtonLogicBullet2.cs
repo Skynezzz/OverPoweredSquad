3b5fba9d67fe0cd6d20120ff804f461108bbb010
Kindhee <117281537+Kindhee@users.noreply.github.com>
2024-04-10 10:54:59 +0200

    mask level

---
+++ Assets/Scripts/MaskTutoLogic/ButtonLogicBullet2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLogicBullet2 : MonoBehaviour
+{
+    public GameObject button1;
+
+    private ButtonBullet b1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        b1 = button1.GetComponent<ButtonBullet>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(b1.isTriggeredBullet)
+        {
+            Destroy(GameObject.Find("Door2"));
+            Destroy(GameObject.Find("Button 1"));
+            Destroy(GameObject.Find("Button 2"));
+        }
+    }
+}
